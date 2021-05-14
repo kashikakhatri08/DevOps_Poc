5267da394ca1e36b5c0f6ec2ad00b7be8bcfa3eb
kashika <kashika@elektrobit.com>
2021-05-14 14:07:00 +0530

    Remove all api related python file which are now persent in jenkins_api_poc folder

--- Jenkins_poc/file_creator.py
+++
@@ -1,9 +0,0 @@
-# This file creates a file in cwd workspace location
-print('==================== start ========================')
-print('===================================================')
-file = open('example.txt', 'w+')
-print('==================== writing file =================')
-file.write('I am a file created by freestyle job')
-file.close()
-print('===================================================')
-print('==================== end ==========================')
